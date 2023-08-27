f51166d663201d3ff43877433e313f4117a6dc88
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-08-27 15:06:45 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PublicationsModule } from './publications/publications.module';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+
+@Module({
+  imports: [PublicationsModule, PostsModule, MediasModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
