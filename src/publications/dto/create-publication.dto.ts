e4bbc5621158df4bea8595b80ec51ecb922256ca
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-08-27 16:00:43 -0300

    dtos config

--- src/publications/dto/create-publication.dto.ts
+++ src/publications/dto/create-publication.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePublicationDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+export class CreatePublicationDto {
+    @IsNumber()
+    @IsNotEmpty()
+    mediaId: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    postId: number;
+
+    @IsDate()
+    @IsNotEmpty()
+    date: Date
+}
--- src/publications/dto/update-publication.dto.ts
+++ src/publications/dto/update-publication.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePublicationDto } from './create-publication.dto';
+import { IsDate, IsNotEmpty, IsNumber, IsString} from "class-validator";
 
-export class UpdatePublicationDto extends PartialType(CreatePublicationDto) {}
+export class UpdatePublicationDto extends PartialType(CreatePublicationDto) {
+    @IsNumber()
+    @IsNotEmpty()
+    id: number;
+    
+    @IsNumber()
+    @IsNotEmpty()
+    mediaId: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    postId: number;
+
+    @IsDate()
+    @IsNotEmpty()
+    date: Date
+}
